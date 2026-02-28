92d29e749bc19d361adef5fdec50a7dcc7f88fc1
Justin Tan <justin.jia.tian@gmail.com>
2026-02-28 20:09:41 +0000

    database

--- evolution/database.py
+++ evolution/database.py
@@ -1,7 +1,7 @@
 import ast, re, os, time
 import multiprocessing
 import asyncio
-import traceback
+import traceback, logging
 
 import numpy as np
 
@@ -12,6 +12,7 @@ from copy import copy
 from typing import Sequence, Any, Dict, Tuple
 _SHUTDOWN = "___SHUTDOWN___"
 METRIC_NAME = "n_solved_eval"
+NOISE_SCALE = 1e-5
 
 # custom imports
 from evolution import code_types, code_utils, code_parser
@@ -23,7 +24,7 @@ class database_config(object):
     cluster_sampling_temperature_init: float = 0.1
     cluster_sampling_temperature_period: int = 30_000
 
-def _get_score(metrics: Dict, score_key: str) -> float:
+def _get_score(metrics: Dict, score_key: str = METRIC_NAME) -> float:
     # higher is better
     return metrics.get(score_key, 0.0)
 
@@ -45,18 +46,24 @@ class ProgramDatabase:
                         for _ in range(config.num_islands)]
         self.best_score_per_island = [float('-inf')] * config.num_islands
         self.best_program_per_island = [None] * config.num_islands
+        self.best_metrics_per_island = [{}] * config.num_islands
     
     def generate_prompts(self):
         island_id = np.random.randint(self.config.num_islands)
         code, version_generated = self.islands[island_id].generate_prompt()
         return code_types.Prompt(code=code, island_id=island_id, version_generated=version_generated)
 
-    def _register_program_in_island(self):
-        return
+    def _register_program_in_island(self, program, island_id, metrics):
+        self.islands[island_id]._register_program(program, metrics)
+        score = _get_score(metrics, self.eval_metric_name)
+        if score > self.best_score_per_island[island_id]:
+            self.best_score_per_island[island_id] = score
+            self.best_program_per_island[island_id] = program
+            self.best_metrics_per_island[island_id] = metrics
 
     def _register_program(self, program, island_id, metrics):
         score = _get_score(metrics, self.eval_metric_name)
-        if island_id is None:
+        if island_id is None:  # start: seed all islands with program
             for i in range(self.config.num_islands):
                 self._register_program_in_island(program, i, metrics)
         else:
@@ -69,7 +76,15 @@ class ProgramDatabase:
     
     def reset_islands(self):
         """Reset worst-performing half with individuals drawn from better half."""
-        return
+        idx_sorted_by_score = np.argsort(self.best_score_per_island + NOISE_SCALE * np.random.randn(self.config.num_islands))
+        reset_n = self.config.num_islands // 2
+        good_pool = idx_sorted_by_score[reset_n:]
+        reset_pool = idx_sorted_by_score[:reset_n]
+        for i in reset_pool:
+            self.islands[i] = Island(self.template, self.function_to_evolve, self.config.functions_per_prompt)
+            seed_id = np.random.choice(good_pool)
+            self.best_score_per_island[i] = -float('inf')
+            self._register_program_in_island(self.best_program_per_island[seed_id], i, self.best_metrics_per_island[seed_id])
 
 class Island:
     """Subpopulation of programs.
